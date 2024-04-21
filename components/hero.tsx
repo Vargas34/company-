bd2851c636c112d98b881cd6be58a19ad870b171
vargas23 <106464730+Vargas34@users.noreply.github.com>
2024-04-21 01:41:43 -0400

    Update hero.tsx
    ubah judul

--- components/hero.tsx
+++ components/hero.tsx
@@ -25,7 +25,7 @@ export default function Hero() {
           {/* Section header */}
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-16">
             <h1 className="h1 mb-4" data-aos="fade-up">Pamungkas Company</h1>
-            <p className="text-xl text-gray-400 mb-8" data-aos="fade-up" data-aos-delay="200">We provide services for creating Websites, IoT, Landing Pages,.</p>
+            <p className="text-xl text-gray-400 mb-8" data-aos="fade-up" data-aos-delay="200">We provide services for creating Websites, IoT, Mobile Apps.</p>
             <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center">
             </div>
           </div>
