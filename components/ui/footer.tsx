833e5178acd49da703d9d777b086b0c63fe92939
vargas23 <106464730+Vargas34@users.noreply.github.com>
2024-04-21 01:26:18 -0400

    Update footer.tsx
    ubah teks

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -31,7 +31,7 @@ export default function Footer() {
                 <h6 className="text-gray-200 font-medium mb-1">Products</h6>
                 <ul>
                   <li className="mb-1">
-                    <Link href="/" className="text-gray-400 hover:text-gray-100 transition duration-150 ease-in-out">Web Develeper</Link>
+                    <Link href="/" className="text-gray-400 hover:text-gray-100 transition duration-150 ease-in-out">Web Developer</Link>
                   </li>
                   <li className="mb-1">
                     <Link href="/" className="text-gray-400 hover:text-gray-100 transition duration-150 ease-in-out">ioT </Link>
