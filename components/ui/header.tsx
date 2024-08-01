59f0c0d13e1ac40cc4ec169eb20a93bac102cc4c
Vargas34 <vargasbrja2342@gmail.com>
2024-08-02 02:11:03 +0700

    update package

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -19,21 +19,7 @@ export default function Header() {
           
           <nav className="hidden md:flex md:grow">
             
-            <ul className="flex grow justify-end flex-wrap items-center">
-              <li>
-                <Link
-                  href="/#"
-                  className="font-medium text-purple-600 hover:text-gray-200 px-4 py-3 flex items-center transition duration-150 ease-in-out"
-                >
-                  Masuk
-                </Link>
-              </li>
-              <li>
-                <Link href="/#" className="btn-sm text-white bg-green-600 hover:bg-green-700 ml-3">
-                  Daftar
-                </Link>
-              </li>
-            </ul>
+          
           </nav>
 
           <MobileMenu />
